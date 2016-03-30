5363bc64c53765c21010cb01eff03bafed77b79d
stasa <magarence@gmail.com>
2016-03-31 01:33:24 +0200

    prva izmena

---
+++ Tests/WrapperTest.php
@@ -0,0 +1,8 @@
+require_once dirname(__FILE__) . '/../Wrapper.php';
+
+class WrapperTest extends PHPUnit_Framework_TestCase {
+
+	function testCanCreateAWrapper() {
+		$wrapper = new Wrapper();
+	}
+}
