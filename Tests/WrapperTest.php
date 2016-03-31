aafda3e03e77867b0402f35c1ae2632fa839c332
stasa <magarence@gmail.com>
2016-03-31 02:19:21 +0200

    finali kod

--- Tests/WrapperTest.php
+++ Tests/WrapperTest.php
@@ -1,8 +1,68 @@
+<?php
+
 require_once dirname(__FILE__) . '/../Wrapper.php';
 
 class WrapperTest extends PHPUnit_Framework_TestCase {
 
-	function testCanCreateAWrapper() {
-		$wrapper = new Wrapper();
+	private $wrapper;
+	
+	function setUp() {
+		$this->wrapper = new Wrapper();
 	}
+
+	function testItEmpty() {
+		$this->assertEquals('', $this->wrapper->wrap('', 0));
+	}
+	
+	function testItDoesNotWrapAShortEnoughWord() {
+		$textToBeParsed = 'word';
+		$maxLineLength = 5;
+		$this->assertEquals('word', $this->wrapper->wrap($textToBeParsed, $maxLineLength));
+	}
+	
+	function testItWrapsAWordLongerThanLineLength() {
+		$textToBeParsed = 'alongword';
+		$maxLineLength = 5;
+		$this->assertEquals("along\nword", $this->wrapper->wrap($textToBeParsed, $maxLineLength));
+	}
+	
+	function testItWrapsAWordServeralTimesIfItsTooLong() {
+		$textToBeParsed = 'averyverylongword';
+		$maxLineLength = 5;
+		$this->assertEquals("avery\nveryl\nongwo\nrd", $this->wrapper->wrap($textToBeParsed, $maxLineLength));
+	}
+	
+	function testItWrapsTwoWordsWhenSpaceAtTheEndOfLine() {
+		$textToBeParsed = 'word word';
+		$maxLineLength = 5;
+		$this->assertEquals("word\nword", $this->wrapper->wrap($textToBeParsed, $maxLineLength));
+	}
+	
+	function testItWrapsTwoWordsWhenLineEndIsAfterFirstWord() {
+		$textToBeParsed = 'word word';
+		$maxLineLength = 7;
+		$this->assertEquals("word\nword", $this->wrapper->wrap($textToBeParsed, $maxLineLength));
+	}
+	
+	function testItWraps3WordsOn2Lines() {
+		$textToBeParsed = 'word word word';
+		$maxLineLength = 12;
+		$this->assertEquals("word word\nword", $this->wrapper->wrap($textToBeParsed, $maxLineLength));
+	}
+	
+	function testItWraps2WordsOn3Lines() {
+		$textToBeParsed = 'word word';
+		$maxLineLength = 3;
+		$this->assertEquals("wor\nd\nwor\nd", $this->wrapper->wrap($textToBeParsed, $maxLineLength));
+	}
+	
+	function testItWraps2WordsAtBoundry() {
+		$textToBeParsed = 'word word';
+		$maxLineLength = 4;
+		$this->assertEquals("word\nword", $this->wrapper->wrap($textToBeParsed, $maxLineLength));
+	}
+
+
 }
+
+?>
